d4387ff0f4ed808f5f09539ab6e9568fdc5ecb51
ethframe <ethframe@gmail.com>
2022-03-30 23:11:48 +0300

    New release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ethframe'
 author = 'ethframe'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
