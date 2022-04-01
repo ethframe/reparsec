172841ab897935ba2bb3eee472870c1faabc5984
ethframe <ethframe@gmail.com>
2022-04-02 00:18:15 +0300

    New release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ethframe'
 author = 'ethframe'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
@@ -64,6 +64,13 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options= {
+    'github_button': True,
+    'github_repo': 'reparsec',
+    'github_user': 'ethframe',
+    'github_type': 'star',
+}
 
 
 # -- Extension configuration -------------------------------------------------
